8b8bdb21efe092c0c81346c6be65c7733ca111f0
ikende <henryfan@msn.com>
2019-12-29 19:14:39 +0800

    add xrpc jwt and wpf samples

---
+++ XRPC.WPF/Server/JwtHelper.cs
@@ -0,0 +1,111 @@
+﻿using BeetleX.EventArgs;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server
+{
+    public class JWTHelper
+    {
+        public const string TOKEN_KEY = "Token";
+
+        private string mIssuer = null;
+
+        private string mAudience = null;
+
+        private SecurityKey mSecurityKey;
+
+        private SigningCredentials mSigningCredentials;
+
+        private TokenValidationParameters mTokenValidation = new TokenValidationParameters();
+
+        private JwtSecurityTokenHandler mJwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+        public JWTHelper(string issuer, string audience, byte[] key)
+        {
+            mIssuer = issuer;
+            mAudience = audience;
+            mSecurityKey = new SymmetricSecurityKey(key);
+            if (string.IsNullOrEmpty(mIssuer))
+            {
+                mTokenValidation.ValidateIssuer = false;
+            }
+            else
+            {
+                mTokenValidation.ValidIssuer = mIssuer;
+            }
+            if (string.IsNullOrEmpty(mAudience))
+            {
+                mTokenValidation.ValidateAudience = false;
+            }
+            else
+            {
+                mTokenValidation.ValidAudience = mAudience;
+            }
+            mTokenValidation.IssuerSigningKey = mSecurityKey;
+            mSigningCredentials = new SigningCredentials(mSecurityKey, SecurityAlgorithms.HmacSha256);
+            Expires = 60 * 24;
+        }
+
+        public int Expires { get; set; }
+
+        public string CreateToken(string name, string role, int timeout = 60)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim("Name", name));
+            claimsIdentity.AddClaim(new Claim("Role", role));
+            var item = mJwtSecurityTokenHandler.CreateEncodedJwt(mIssuer, mAudience, claimsIdentity, DateTime.Now.AddMinutes(-5),
+                DateTime.Now.AddMinutes(timeout), DateTime.Now,
+               mSigningCredentials);
+            return item;
+        }
+
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            return mJwtSecurityTokenHandler.ValidateToken(token, mTokenValidation, out var securityToken);
+        }
+
+        public UserInfo GetUserInfo(string token)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(token))
+                {
+                    UserInfo userInfo = new UserInfo();
+                    var info = ValidateToken(token);
+                    ClaimsIdentity identity = info?.Identity as ClaimsIdentity;
+                    userInfo.Name = identity?.Claims?.FirstOrDefault(c => c.Type == "Name")?.Value;
+                    userInfo.Role = identity?.Claims?.FirstOrDefault(c => c.Type == "Role")?.Value;
+                    return userInfo;
+                }
+            }
+            catch {  
+            }
+            return null;
+        }
+
+        public class UserInfo
+        {
+            public string Name;
+
+            public string Role;
+        }
+
+        public static JWTHelper Default
+        {
+            get;
+            set;
+        }
+
+        public static void Init()
+        {
+            byte[] key = new byte[128];
+            new Random().NextBytes(key);
+            Default = new JWTHelper("xrpc", "xrpc", key);
+        }
+    }
+}
---
+++ XRPC.WPF/Server/Program.cs
@@ -0,0 +1,77 @@
+﻿using BeetleX.XRPC.Hosting;
+using BeetleX.XRPC.Packets;
+using Microsoft.Extensions.Hosting;
+using Northwind.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventNext;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.UseXRPC(s =>
+                {
+                    JWTHelper.Init();
+                    s.ServerOptions.LogLevel = BeetleX.EventArgs.LogType.Trace;
+                    s.ServerOptions.DefaultListen.Port = 9090;
+                    s.RPCOptions.ParameterFormater = new JsonPacket();//default messagepack
+                },
+                    typeof(Program).Assembly);
+            });
+            builder.Build().Run();
+        }
+    }
+
+    public interface IDataService
+    {
+        Task<string> Login(string name, string pwd);
+
+        Task<List<Employee>> List();
+    }
+    [Service(typeof(IDataService))]
+    [TokenFilter]
+    public class DataServiceImpl : IDataService
+    {
+        public Task<List<Employee>> List()
+        {
+            return DataHelper.Defalut.Employees.ToTask();
+        }
+
+        [SkipActionFilter(typeof(TokenFilter))]
+        public Task<string> Login(string name, string pwd)
+        {
+            string token = null;
+            if (name == "admin" && pwd == "123456")
+                token = JWTHelper.Default.CreateToken(name, "admin");
+            return token.ToTask();
+
+        }
+    }
+
+    public class TokenFilter : ActionFilterAttribute
+    {
+        public override bool Executing(EventCenter center, EventActionHandler handler, IEventInput input, IEventOutput output)
+        {
+            string token = null;
+            input.Properties?.TryGetValue("token", out token);
+            var user = JWTHelper.Default.GetUserInfo(token);
+            if (user!=null)
+            {
+                return base.Executing(center, handler, input, output);
+            }
+            else
+            {
+                output.EventError = EventError.InnerError;
+                output.Data = new object[] { "操作错误，无权操作相关资源！" };
+                return false;
+            }
+        }
+    }
+
+}
