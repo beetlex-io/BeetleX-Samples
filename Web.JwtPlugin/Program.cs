355911fc2d2a565f0dc7f3272f23323b654c6393
ikende <henryfan@msn.com>
2020-03-09 22:05:00 +0800

    add webapi jwt plugin

---
+++ Web.JwtPlugin/Program.cs
@@ -0,0 +1,46 @@
+﻿using BeetleX.FastHttpApi;
+using BeetleX.FastHttpApi.Hosting;
+using BeetleX.FastHttpApi.Jwt;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Web.JwtPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+             .ConfigureServices((hostContext, services) =>
+             {
+                 //BeetleX.FastHttpApi.Hosting
+                 services.UseBeetlexHttp(o =>
+                 {
+                     o.Port = 80;
+                     o.LogToConsole = true;
+                 },
+                 http =>
+                 {
+                     http.UseJWT();//BeetleX.FastHttpApi.Jwt
+                 },
+                 typeof(Program).Assembly);
+             });
+            builder.Build().Run();
+        }
+    }
+    [Controller]
+    public class Home
+    {
+        
+        public Object GetTime()
+        {
+            return DateTime.Now;
+        }
+        [AuthMark(AuthMarkType.NoValidation)]
+        public void Login(string name,string pwd,IHttpContext context)
+        {
+            context.SetJwtToken(name, "admin");
+        }
+    }
+
+}
