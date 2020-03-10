17a03c8369a4409028229a0bde941fe0dffbc585
ikende <henryfan@msn.com>
2020-03-10 11:08:03 +0800

    add delegate publish/subscribe

---
+++ XRPC.DelegateSubscribe/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using BeetleX.XRPC.Clients;
+using BeetleX.XRPC.Packets;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static XRPCClient client;
+        static void Main(string[] args)
+        {
+            client = new XRPCClient("192.168.1.18", 9090);
+            //client.PingTimeout = 5;
+            client.Options.ParameterFormater = new JsonPacket();
+          
+            client.TimeOut = 100000;
+            //  TestTaskResultDelegate(client);
+            //UnitTest();
+            var task = client.Subscribe<Action<DateTime>>(d =>
+            {
+                Console.WriteLine(d);
+            });
+            System.Threading.Thread.Sleep(-1);
+        }
+    }
+}
---
+++ XRPC.DelegateSubscribe/Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using BeetleX.EventArgs;
+using BeetleX.XRPC;
+using BeetleX.XRPC.Hosting;
+using BeetleX.XRPC.Packets;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static XRPCServer mXRPCServer;
+
+        static void Main(string[] args)
+        {
+
+            var builder = new HostBuilder()
+              .ConfigureServices((hostContext, services) =>
+              {
+                  services.UseXRPC(s =>
+                  {
+                      s.ServerOptions.LogLevel = LogType.Warring;
+                      s.RPCOptions.ParameterFormater = new JsonPacket();
+                  },
+                  rpc =>
+                  {
+                      Task.Run(() => OnPublish(rpc));
+
+                  },
+                  typeof(Program).Assembly);
+              });
+
+            builder.Build().Run();
+        }
+
+        static void OnPublish(XRPCServer server)
+        {
+            while (true)
+            {
+                server.Publish<Action<DateTime>>()(DateTime.Now);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
