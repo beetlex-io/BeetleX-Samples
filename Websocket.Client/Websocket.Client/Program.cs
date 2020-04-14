86bf586c864040bf603439aa0653fb2f033dde4a
Henry <henryfan@msn.com>
2020-04-14 19:15:32 +0800

    add websocket client sample

---
+++ Websocket.Client/Websocket.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Websocket.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+
+            Console.Read();
+        }
+
+        static async void Test()
+        {
+            var ws = new BeetleX.Http.WebSockets.TextClient("ws://echo.websocket.org");
+
+            var result = await ws.ReceiveFrom("ws hello henry");
+            Console.WriteLine(result);
+
+            var wss = new BeetleX.Http.WebSockets.TextClient("wss://echo.websocket.org");
+
+            result = await wss.ReceiveFrom("wss hello henry");
+            Console.WriteLine(result);
+        }
+    }
+}
