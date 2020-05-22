90746c1d724d2d4c636560453af1f094247b7ed7
Henry <henryfan@msn.com>
2020-05-22 10:38:20 +0800

    add web admin sample

---
+++ Web.Admin/Program.cs
@@ -0,0 +1,32 @@
+﻿using BeetleX.FastHttpApi.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Web.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] obj = Environment.GetCommandLineArgs();
+            var builder = new HostBuilder()
+             .ConfigureServices((hostContext, services) =>
+             {
+                 services
+                 .UseBeetlexHttp(o =>
+                 {
+                     o.Port = 80;
+                     o.LogToConsole = true;
+                     o.LogLevel = BeetleX.EventArgs.LogType.Info;
+                     o.SetDebug();
+                 },
+                 HttpServer =>
+                 {
+
+                 },
+                 typeof(Program).Assembly, typeof(BeetleX.FastHttpApi.Admin._Admin).Assembly);
+             });
+            builder.Build().Run();
+        }
+    }
+}
