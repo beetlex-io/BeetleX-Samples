86059da49f0725cee27ad258b6267902f4cb11e7
Henry <henryfan@msn.com>
2021-01-02 22:00:01 +0800

    add xamarin use xrpc sample

---
+++ BeetleX.XRPC.Xamarin/ServerApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using BeetleX.XRPC.Hosting;
+using BeetleX.XRPC.Packets;
+using BeetleX.XRPC;
+using EventNext;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+           .ConfigureServices((hostContext, services) =>
+           {
+               services.UseXRPC(s =>
+               {
+                   s.ServerOptions.LogLevel = BeetleX.EventArgs.LogType.Warring;
+                   s.ServerOptions.DefaultListen.Port = 9090;
+                   s.RPCOptions.ParameterFormater = new JsonPacket();//default messagepack
+               },
+               s =>
+               {
+                   s.AddDelegate<Func<Task<string>>>(() =>
+                   {
+                       return Task.FromResult($"{Environment.OSVersion} {DateTime.Now}");
+                   });
+               }
+               , typeof(Program).Assembly);
+           });
+            builder.Build().Run();
+        }
+    }
+
+    public interface IUser
+    {
+        Task<string> Login(string name, string pwd);
+    }
+
+    [Service(typeof(IUser))]
+    public class HelloImpl : IUser
+    {
+        public Task<string> Login(string name, string pwd)
+        {
+            var context = XRPCServer.EventToken;
+            GetClientTime(context.Server, context.Session);
+            Console.WriteLine($"{name} login at {context.Session.RemoteEndPoint}");
+            return $"{name} login {DateTime.Now}".ToTask();
+        }
+
+        private async Task GetClientTime(XRPCServer server, BeetleX.ISession session)
+        {
+            var action = server.Delegate<Func<Task<string>>>(session);
+            while (true)
+            {
+                var result = await action();
+                Console.WriteLine($"{result}@{session.RemoteEndPoint}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+
+}
---
+++ BeetleX.XRPC.Xamarin/XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using BeetleX.XRPC.Packets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            mClient = new BeetleX.XRPC.Clients.XRPCClient("192.168.1.18", 9090);
+            mClient.Options.ParameterFormater = new JsonPacket();
+            mUser = mClient.Create<IUser>();
+            mClient.AddDelegate<Func<Task<string>>>(() =>
+            {
+                return Task.FromResult($"{Environment.OSVersion} {DateTime.Now}");
+            });
+            mGetTime = mClient.Delegate<Func<Task<string>>>();
+        }
+
+        private Func<Task<string>> mGetTime;
+
+        private static IUser mUser;
+
+        private static BeetleX.XRPC.Clients.XRPCClient mClient;
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userName.Text))
+                {
+                    await DisplayAlert("Error", "Enter you name!", "OK");
+                    return;
+                }
+                var result = await mUser.Login(userName.Text, userPwd.Text);
+                this.layoutGrid.IsVisible = false;
+                this.cmdLogin.IsVisible = false;
+                OnGetTime();
+            }
+            catch (Exception e_)
+            {
+                await DisplayAlert("Error", e_.Message, "OK");
+            }
+        }
+
+        private async Task OnGetTime()
+        {
+            while (true)
+            {
+                var result = await mGetTime();
+                txtResult.Text = result;
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
