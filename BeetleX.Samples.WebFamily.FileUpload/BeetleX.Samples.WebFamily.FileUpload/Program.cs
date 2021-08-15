1ae53acc9b09149a4b9f871c1aa6cf3ba40ac23d
Henry <henryfan@msn.com>
2021-08-15 20:31:03 +0800

    no message

---
+++ BeetleX.Samples.WebFamily.FileUpload/BeetleX.Samples.WebFamily.FileUpload/Program.cs
@@ -0,0 +1,34 @@
+﻿using BeetleX.WebFamily;
+using System;
+
+namespace BeetleX.Samples.WebFamily.FileUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost host = new WebHost();
+            WebHost.HomeModel = "webfamily-files";
+            
+
+            host.Setting(o =>
+            {
+
+                o.SetDebug();
+                o.Port = 80;
+                o.LogLevel = EventArgs.LogType.Info;
+                o.LogToConsole = true;
+            })
+                .UseFileManager(o =>
+                {
+                    o.MaxSize = 1024 * 1024 * 1024;
+                })
+            .UseElement(PageStyle.Element)
+            .Initialize((http, vue, resoure) =>
+            {
+                vue.Debug();
+            }).Run();
+        }
+    }
+}
+
