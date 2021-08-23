145001596b983a7251505836d4bbf22e10d7ad3a
Henry <henryfan@msn.com>
2021-08-23 09:22:05 +0800

    add elasticsearch sample

---
+++ BeetleX.Samples.WebFamily.ElasticSearch/BeetleX.Samples.WebFamily.ElasticSearch/Program.cs
@@ -0,0 +1,47 @@
+﻿using BeetleX.WebFamily;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using BeetleX.WebFamily.Searches;
+using BeetleX.FastHttpApi.EFCore.Extension;
+using BeetleX.FastHttpApi;
+
+namespace BeetleX.Samples.WebFamily.ElasticSearch
+{
+    [BeetleX.FastHttpApi.Controller]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost host = new WebHost();
+            host.RegisterComponent<Program>();
+            host.UserElasticSearch("test_query_string", "http://192.168.2.19:9200");
+            host.UseEFCore<BlogDBContext>();
+            host.Setting(o =>
+            {
+                o.Port = 80;
+                o.LogLevel = EventArgs.LogType.Info;
+                o.LogToConsole = true;
+            })
+            .Run();
+        }
+
+        public async Task<object> Search(IHttpContext context)
+        {
+            var search = context.Server.GetDocument().CreateSearch(0, 20);
+            search.QueryText = "beetlex websocket";
+            search.Highlight = true;
+            var items = await search.Execute();
+            return items;
+        }
+
+        public async Task Import(IHttpContext context, EFCoreDB<BlogDBContext> db)
+        {
+            var doc = context.Server.GetDocument();
+            var items = from a in db.DBContext.Posts select a;
+            foreach (var item in items)
+                await doc.Put(new Document { Category = item.Project, Content = item.Content, CreateTime = item.CreateTime, ID = item.ID, Tag = item.Tag, Title = item.Title });
+
+        }
+    }
+}
---
+++ BeetleX.Samples.WebFamily.ElasticSearch/BeetleX.Samples.WebFamily.ElasticSearch/db.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetleX.Samples.WebFamily.ElasticSearch
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext()
+        {
+        }
+
+        public BlogDBContext(DbContextOptions<BlogDBContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Filename=BeetlexBlog.db");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
+
+        public DbSet<Post> Posts { get; set; }
+
+
+    }
+
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        public string ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Project { get; set; }
+
+        public string Tag { get; set; }
+
+        public string SourceUrl { get; set; }
+
+        public string Content { get; set; }
+
+        public string Digest { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public string User { get; set; }
+
+        public string NickName { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int Level { get; set; }
+    }
+}
