46cc433316f80bef9fa46db73c16cada9c17fcfa
Henry <henryfan@msn.com>
2021-08-26 10:59:23 +0800

    add Markdown editor

---
+++ BeetleX.Samples.WebFamily.MarkdownEditor/BeetleX.Samples.WebFamily.MarkdownEditor/Program.cs
@@ -0,0 +1,34 @@
+﻿using BeetleX.WebFamily;
+using System;
+
+namespace BeetleX.Samples.WebFamily.MarkdownEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            WebHost host = new WebHost();
+            WebHost.Title = "Beetlex WebFamily";
+            WebHost.HomeModel = "webfamily-md";
+            WebHost.HomeName = "Markdown";
+            WebHost.Login = false;
+            host.RegisterComponent<Program>();
+            host.UseToastUIEditor();
+            host.Setting(o =>
+            {
+                o.SetDebug();
+                o.Port = 80;
+                o.LogLevel = EventArgs.LogType.Info;
+                o.LogToConsole = true;
+            })
+            .UseElement(PageStyle.Element)
+            .Initialize((http, vue, resoure) =>
+            {
+                resoure.AddCss("website.css");
+                vue.Debug();
+            }).Run();
+        }
+    }
+}
