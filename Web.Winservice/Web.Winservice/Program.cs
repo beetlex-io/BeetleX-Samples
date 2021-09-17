e8a296f92704890963b5e16dcb5c0fa07a6f8fdc
Henry <henryfan@msn.com>
2021-09-17 14:31:44 +0800

    web windows service

---
+++ Web.Winservice/Web.Winservice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BeetleX.FastHttpApi;
+using BeetleX.FastHttpApi.Hosting;
+namespace Web.Winservice
+{
+    class Program
+    {
+        private static HttpServer mServer;
+
+        static void Main(string[] args)
+        {
+            mServer = new HttpServer(80);
+            mServer.IsWindowsServices = true;
+            mServer.Setting((service, option) =>
+            {
+                option.LogToConsole = true;
+                option.WriteLog = true;
+                option.LogLevel = BeetleX.EventArgs.LogType.Info;
+            });
+            mServer.RegisterComponent<Home>();
+            mServer.Run();
+        }
+    }
+    [Controller]
+    public class Home
+    {
+        public object Hello(string name)
+        {
+            return $"hello {name}";
+        }
+    }
+}
