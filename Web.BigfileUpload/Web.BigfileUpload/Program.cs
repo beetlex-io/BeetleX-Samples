02275120c122a79024c60a0b809b972a35492866
Henry <henryfan@msn.com>
2021-10-01 10:47:05 +0800

    webapi upload big files

---
+++ Web.BigfileUpload/Web.BigfileUpload/Program.cs
@@ -0,0 +1,60 @@
+﻿using BeetleX.EventArgs;
+using BeetleX.FastHttpApi;
+using BeetleX.FastHttpApi.Data;
+using BeetleX.WebFamily;
+using System;
+using System.IO;
+
+namespace Web.BigfileUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost host = new WebHost();
+            host.RegisterComponent<Program>()
+            .Setting(o =>
+            {
+                o.SetDebug();
+                o.Port = 80;
+                o.LogLevel = LogType.Warring;
+                o.LogToConsole = true;
+            })
+            .Initialize((http, vue, rec) =>
+            {
+                rec.AddCss("website.css");
+                vue.Debug();
+            }).Run();
+        }
+    }
+
+    [Controller]
+    class Webapi
+    {
+        private System.Collections.Concurrent.ConcurrentDictionary<string, System.IO.Stream> mStreams
+            = new System.Collections.Concurrent.ConcurrentDictionary<string, System.IO.Stream>(StringComparer.OrdinalIgnoreCase);
+        [Put]
+        [NoDataConvert]
+        public void Upload(string name, bool first, bool eof, IHttpContext context)
+        {
+            Console.WriteLine($"{name}-{first}-{eof}-{context.Request.Stream.Length}");
+            Stream stream;
+            if (first)
+            {
+                stream = System.IO.File.Create(name);
+                mStreams[name] = stream;
+            }
+            else
+            {
+                mStreams.TryGetValue(name, out stream);
+            }
+            context.Request.Stream.CopyTo(stream);
+            if (eof)
+            {
+                stream.Flush();
+                stream.Dispose();
+                mStreams.TryRemove(name, out stream);
+            }
+        }
+    }
+}
---
+++ Web.BigfileUpload/Web.BigfileUpload/views/vue/upload.vue
@@ -0,0 +1,34 @@
+﻿<div>
+    <div class="row">
+        <div class="form-group">
+            <label for="exampleInputEmail1"></label>
+            <input type="file" class="form-control" placeholder="name" multiple @change="onSelectFile">
+        </div>
+    </div>
+    <div class="row">
+        <div v-for="item in items" class="upload-item">
+            <div>{{item.name}}[{{item.loadSize}}/{{item.size}}]</div>
+            <el-progress :stroke-width="2" :percentage="item.percent" type="line" :show-text="false"></el-progress>
+        </div>
+
+    </div>
+</div>
+<script>
+    {
+        data() {
+            return { items: [] }
+        },
+        methods: {
+            onSelectFile(e) {
+                var files = e.target.files;
+                if (files.length > 0) {
+                    for (i = 0; i < files.length; i++) {
+                        var item = new FileUploader(files[i], 1024 * 128);
+                        this.items.push(item);
+                        item.upload("Upload");
+                    }
+                }
+            },
+        }
+    }
+</script>
