9e779a1593ea1b887dd6884903b22855577b4312
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2021-11-26 13:45:11 +0800

    add code benchmark sample

---
+++ CodeBenchmark.Sample/CodeBenchmark.Sample/Program.cs
@@ -0,0 +1,57 @@
+﻿using BeetleX.Http.Clients;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeBenchmark.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Benchmark benchmark = new Benchmark();
+            benchmark.Register(typeof(Program).Assembly);
+            benchmark.Start(80,true);
+            
+            Console.Read();
+        }
+    }
+    [System.ComponentModel.Category("HTTP")]
+    class BaiduGet : IExample
+    {
+        public void Dispose()
+        {
+        }
+
+        public async Task Execute()
+        {
+            var result = await "https://www.baidu.com".FormUrlRequest().Get();
+        }
+
+        public void Initialize(Benchmark benchmark)
+        {
+
+        }
+
+
+    }
+
+    [System.ComponentModel.Category("HTTP")]
+    class WYGet : IExample
+    {
+        public void Dispose()
+        {
+        }
+
+        public async Task Execute()
+        {
+            var result = await "https://www.163.com".FormUrlRequest().Get();
+        }
+
+        public void Initialize(Benchmark benchmark)
+        {
+
+        }
+
+
+    }
+}
