3366364f0f9475b96b3026f6cc97b4d85ba70535
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2021-12-07 13:25:59 +0800

    no message

--- WebSocket.Hello/WebSocket.Hello/Program.cs
+++ WebSocket.Hello/WebSocket.Hello/Program.cs
@@ -1,11 +1,16 @@
-﻿using BeetleX.FastHttpApi.Hosting;
+﻿using BeetleX.FastHttpApi;
+using BeetleX.FastHttpApi.Hosting;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Threading.Tasks;
 
 namespace WebSocket.Hello
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
             var builder = new HostBuilder()
@@ -18,6 +23,13 @@ namespace WebSocket.Hello
                         o.Port = 80;
                         o.SetDebug();
                         o.LogLevel = BeetleX.EventArgs.LogType.Warring;
+                    }, http =>
+                    {
+                        http.WebSocketConnect = (o, e) =>
+                        {
+                            //e.Cancel = true;
+                            //e.Error = new UpgradeWebsocketError(400, "websocket error!");
+                        };
                     },
                     typeof(Program).Assembly);
                 });
@@ -25,11 +37,31 @@ namespace WebSocket.Hello
         }
     }
     [BeetleX.FastHttpApi.Controller]
-    public class Home
+    public class Home : BeetleX.FastHttpApi.IController
     {
         public string Hello(string name)
         {
             return $"hello {name} {DateTime.Now}";
+        }
+
+        private HttpApiServer mServer;
+
+        [NotAction]
+        public void Init(HttpApiServer server, string path)
+        {
+            mServer = server;
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (this.mServer.BaseServer != null)
+                    {
+                        var items = this.mServer.GetWebSockets();
+                        Console.WriteLine($"WS-Online:{items.Count}");
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
         }
     }
 }
