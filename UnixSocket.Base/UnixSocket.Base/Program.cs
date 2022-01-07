190e49628ebc2425679d40e232c4978b5d567062
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2022-01-07 21:55:44 +0800

    unix socket sample

---
+++ UnixSocket.Base/UnixSocket.Base/Program.cs
@@ -0,0 +1,55 @@
+﻿using BeetleX;
+using BeetleX.Clients;
+using BeetleX.EventArgs;
+using System;
+using System.Threading.Tasks;
+
+namespace UnixSocket.Base
+{
+    class Program : BeetleX.ServerHandlerBase
+    {
+        //static string mHost = "127.0.0.1";
+
+        static string mHost = "/tmp/beetlex_unixsocket.sock";
+
+        static BeetleX.IServer mServer;
+
+        static AsyncTcpClient mClient;
+        static async Task Main(string[] args)
+        {
+            mServer = BeetleX.SocketFactory.CreateTcpServer<Program>();
+            mServer.Options.DefaultListen.Host = mHost;
+            mServer.Options.DefaultListen.Port = 9090;
+            mServer.Open();
+            mClient = SocketFactory.CreateClient<AsyncTcpClient>(mHost, 9090);
+            mClient.DataReceive = (c, e) =>
+            {
+                if (e.Stream.ToPipeStream().TryReadLine(out string line))
+                {
+                    Console.WriteLine("receive:" + line); 
+                    Enter();
+                }
+            };
+            Enter();
+            await Task.Delay(-1);
+        }
+        static void Enter() {
+            Console.Write("enter name:");
+            var name = Console.ReadLine();
+            mClient.Stream.ToPipeStream().WriteLine(name);
+            mClient.Stream.Flush();
+        }
+
+        public override void SessionReceive(IServer server, SessionReceiveEventArgs e)
+        {
+            base.SessionReceive(server, e);
+            var stream = e.Stream.ToPipeStream();
+            if (stream.TryReadLine(out string line))
+            {
+                stream.WriteLine(line);
+                stream.Flush();
+
+            }
+        }
+    }
+}
---
+++ UnixSocket.Http/UnixSocket.Http/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using BeetleX.FastHttpApi;
+using BeetleX.FastHttpApi.Hosting;
+using BeetleX.Http.Clients;
+
+namespace UnixSocket.Http
+{
+    [BeetleX.FastHttpApi.Controller]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer(8080);
+            server.RegisterComponent<Program>();
+            server.Setting((service, o) =>
+            {
+                o.LogToConsole = true;
+                o.SockFile = "/tmp/beetlex_http.sock";
+            });
+            server.Completed(http =>
+            {
+                Task.Run(Test);
+            });
+            server.Run();
+        }
+        static async Task Test()
+        {
+            var request = new HttpFormUrlClient("http://localhost:8080/GetTime");
+            var response = await request.Get();
+            Console.WriteLine("http socket response:" + response.Body);
+
+            request = new HttpFormUrlClient("/tmp/beetlex_http.sock/GetTime");
+            response = await request.Get();
+            Console.WriteLine("http unix socket response:" + response.Body);
+
+        }
+
+        public object GetTime()
+        {
+            return new TextResult(DateTime.Now.ToString());
+        }
+
+
+    }
+}
