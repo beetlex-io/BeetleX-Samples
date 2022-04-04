aa568247ee647b9bca6745942a1dfc0ab0d2df42
DESKTOP-2N3GSM4\henry <henryfan@msn.com>
2022-04-04 14:11:23 +0800

    add websocket authorization

---
+++ Websocket.Authorization/Websocket.Authorization.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using BeetleX.Http.WebSockets;
+using System;
+using System.Threading.Tasks;
+
+namespace Websocket.Authorization.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new TextClient("ws://localhost");
+            client.Headers["Authorization"] = "beetlex";
+            var result = await client.ReceiveFrom("beetlex");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Websocket.Authorization/Websocket.Authorization/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BeetleX.FastHttpApi.Hosting;
+namespace Websocket.Authorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer(80);
+            server.Setting((service, option) =>
+            {
+                option.LogToConsole = true;
+                option.LogLevel = BeetleX.EventArgs.LogType.Trace;
+            }).Completed(http =>
+            {
+                http.WebSocketConnect += (o, e) =>
+                {
+                    if (e.Request.Authorization != "beetlex")
+                    {
+                        e.Cancel = true;
+                        e.Error = new BeetleX.FastHttpApi.UpgradeWebsocketError(500, "Authorization error!");
+                    }
+                };
+                http.WebSocketReceive = (o, e) =>
+                {
+                    e.ResponseText("beetlex websocket");
+                };
+            }).Run();
+        }
+    }
+}
